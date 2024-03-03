c6e8d9196c22d1d53124b6f9ea0edfb5f1474432
ZaiErb <58526166+goobGoblin@users.noreply.github.com>
2024-03-02 19:44:00 -0800

    Update VisualMedia.js
    Update to remove play controls from videos that arent playing

--- VisualMedia.js
+++ VisualMedia.js
@@ -5,14 +5,17 @@ let currentVideo = null; // Initially set to null
 videos.forEach(video => {
     video.addEventListener('click', () => {
         if (currentVideo && currentVideo !== video) {
-            currentVideo.pause(); // Pause the currently playing video
+            currentVideo.pause(); // Pause the previously playing video
+            currentVideo.removeAttribute('controls'); // Remove controls from previously playing video
         }
         currentVideo = video; // Set the currentVideo to the clicked video
 
         if (video.paused) {
             video.play(); // Play the video if it's paused
+            video.setAttribute('controls', ''); // Show controls when video is played
         } else {
             video.pause(); // Pause the video if it's playing
+            video.removeAttribute('controls'); // Hide controls when video is paused
         }
         // Set the background color of the video container to match the current video
         videoContainer.style.backgroundColor = video.style.backgroundColor;
@@ -23,6 +26,7 @@ videos.forEach(video => {
         videos.forEach(v => {
             if (v !== video) {
                 v.pause();
+                v.removeAttribute('controls'); // Remove controls from other videos
             }
         });
         // Set the background color of the video container to match the current video
